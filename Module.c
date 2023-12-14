ef011a39dd06ab01f082862e90c6e7808ec1745c
Apoorv Seth <sethapoorv724@gmail.com>
2023-12-14 16:47:15 +0530

    bash: line 1: x: command not found 	modified:   Module.c

--- Module.c
+++ Module.c
@@ -1,35 +1,73 @@
-/*Major & Minor number*/
+/*Device file creation*/
 
 #include<linux/kernel.h>
 #include<linux/init.h>
 #include<linux/module.h>
+#include<linux/kdev_t.h>
 #include<linux/fs.h>
+#include<linux/device.h>
 
 
-/*Creating custom Major & Minor number dev, assigning the values*/
+/*Creating Major & Minor number dynamically, not assigning the values below*/
 
-dev_t dev = MKDEV(224,0);
+dev_t dog = 0;
 
-/*Making the initialisation module*/
+/*Making a structure class pointer for storing t he address of the class structure for creating the device file*/
 
-static int __init number_init(void)
+static struct class *device_class;
+
+/*Making function to allocate the Major number dynamically*/
+
+static int __init file_init(void)
 {
-	register_chrdev_region(dev, 1, "Apoorv");
-	pr_info("MAJOR = %d MINOR = %d\n", MAJOR(dev), MINOR(dev));
-	pr_info("Kernel module has been succesfully inserted!\n");
-	return 0;
+        /*Allocating Major number dynamically*/
+
+        if((alloc_chrdev_region(&dog, 0, 1, "Apoorv")) <0 ){
+                pr_err("Oops! Module could not be loaded into the Kernel!\n");
+                return -1;
+	}
+        pr_info("MAJOR = %d MINOR = %d\n", MAJOR(dog), MINOR(dog));
+        pr_info("Major & Minor number has been assigned succesfully!\n");
+
+        /*Creating the class for Device file*/
+
+        if((device_class = class_create(THIS_MODULE, "Seth_class")) == NULL){
+                pr_err("Oops! Class could not be created for the device file, better check your code\n!");
+                goto r_class;
+        }
+        pr_info("Class has been created successfully for the device file!");
+
+        /*Creating the device file for Application*/
+
+        if((device_create(device_class, NULL, dog, NULL, "Seth_device")) == NULL){
+                pr_err("Oops! Device file could not be created, check your code bro!\n");
+                goto r_dev;
+        }
+        pr_info("Kernel module has been created successfully!Congratulations bro!\n");
+        return 0;
+
+r_dev:
+        device_destroy(device_class, dog);
+r_class:
+        unregister_chrdev_region(dog ,1);
+        return-1;
 }
 
-static void  __exit number_exit(void)
+/*Exit function for the module*/
+
+static void  __exit file_exit(void)
 {
-	unregister_chrdev_region(dev, 1);
-	pr_info("Kernel has been succesfully removed!");
+        device_destroy(device_class, dog);
+        class_destroy(device_class);
+        unregister_chrdev_region(dog, 1);
+        pr_info("Kernel has been succesfully removed!");
 }
 
-module_init(number_init);
-module_exit(number_exit);
+module_init(file_init);
+module_exit(file_exit);
 
-MODULE_LICENSE("GPU");
+MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Apoorv Seth");
 MODULE_DESCRIPTION("Module build for creating and assigning Major and Minor numbers");
 MODULE_VERSION("1.0");
+
